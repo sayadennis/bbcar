b79e766d221270d7b33a19a18d4c4d396b082ec3
sayadennis <sayarenedennis@gmail.com>
2023-11-17 15:19:38 -0600

    easily toggle violin plot between De Novo vs. COSMIC

--- 03_statistitcal_tests/mutation/compare_signature_er_positive_vs_negative.py
+++ 03_statistitcal_tests/mutation/compare_signature_er_positive_vs_negative.py
@@ -20,6 +20,7 @@ for sigcat in ['SBS96', 'DBS78', 'ID83']:
     denovo = pd.read_csv(f'{din}/{sigcat}/Suggested_Solution/{sigcat}_De-Novo_Solution/Signatures/{sigcat}_De-Novo_Signatures.txt', sep='\t', index_col=0)
     denovo_exposure = pd.DataFrame(np.dot(samples.values, denovo.values), index=[int(x.split('_')[0]) for x in samples.index], columns=denovo.columns)
     data['De Novo'][sigcat] = denovo_exposure
+    data['COSMIC'][sigcat] = pd.read_csv(f'/projects/b1131/saya/bbcar/data/02a_mutation/08_feature_matrix/cosmic_signature_per_sample_{sigcat}.csv', index_col=0)
 
 ## Load clinical features
 label = pd.read_csv('/projects/b1131/saya/bbcar/data/clinical/bbcar_label_studyid_from_gatk_filenames.csv', index_col=0)
@@ -49,49 +50,36 @@ for i in labels_all.index:
 #### Plot violin plots ####
 ###########################
 
-sbs = data['De Novo']['SBS96']
+sig_type = 'COSMIC'
 
-fig, ax = plt.subplots(figsize=(12,4))
+sbs = data[sig_type]['SBS96']
 
-pos = [1,2,3,5,6,7,9,10,11,13,14,15,17,18,19,21,22,23]
+fig, ax = plt.subplots(figsize=(16,4))
+
+pos = np.arange(sbs.shape[1] * 4)
+pos = pos[pos % 4 != 0]  # define positions to plot (this will look like [1,2,3,5,6,7,...])
+
+plot_data = []
+
+for signame in sbs.columns:
+    for bio_group in ['Control', '0% Negative', '10% Positive']:
+        plot_data.append(sbs.iloc[labels_all['ER'].values==bio_group,:][signame].values)
+
 violins = ax.violinplot(
-    [sbs.iloc[labels_all['ER'].values=='Control',:]['SBS96A'].values,
-    sbs.iloc[labels_all['ER'].values=='0% Negative',:]['SBS96A'].values,
-    sbs.iloc[labels_all['ER'].values=='10% Positive',:]['SBS96A'].values,
-
-    sbs.iloc[labels_all['ER'].values=='Control',:]['SBS96B'].values,
-    sbs.iloc[labels_all['ER'].values=='0% Negative',:]['SBS96B'].values,
-    sbs.iloc[labels_all['ER'].values=='10% Positive',:]['SBS96B'].values,
-
-    sbs.iloc[labels_all['ER'].values=='Control',:]['SBS96C'].values,
-    sbs.iloc[labels_all['ER'].values=='0% Negative',:]['SBS96C'].values,
-    sbs.iloc[labels_all['ER'].values=='10% Positive',:]['SBS96C'].values,
-
-    sbs.iloc[labels_all['ER'].values=='Control',:]['SBS96D'].values,
-    sbs.iloc[labels_all['ER'].values=='0% Negative',:]['SBS96D'].values,
-    sbs.iloc[labels_all['ER'].values=='10% Positive',:]['SBS96D'].values,
-
-    sbs.iloc[labels_all['ER'].values=='Control',:]['SBS96E'].values,
-    sbs.iloc[labels_all['ER'].values=='0% Negative',:]['SBS96E'].values,
-    sbs.iloc[labels_all['ER'].values=='10% Positive',:]['SBS96E'].values,
-
-    sbs.iloc[labels_all['ER'].values=='Control',:]['SBS96F'].values,
-    sbs.iloc[labels_all['ER'].values=='0% Negative',:]['SBS96F'].values,
-    sbs.iloc[labels_all['ER'].values=='10% Positive',:]['SBS96F'].values],
-    pos, widths=0.7, showmedians=True, showextrema=True
+    plot_data, pos, widths=0.7, showmedians=True, showextrema=True
 )
 
-for i in [0,3,6,9,12,15]:
+for i in [x for x in range(len(pos)) if x % 3 == 0]:
     violins['bodies'][i].set_facecolor('blue')
 
-for i in [1,4,7,10,13,16]:
+for i in [x for x in range(len(pos)) if x % 3 == 1]:
     violins['bodies'][i].set_facecolor('green')
 
-for i in [2,5,8,11,14,17]:
+for i in [x for x in range(len(pos)) if x % 3 == 2]:
     violins['bodies'][i].set_facecolor('orange')
 
-ax.set_xticks([2,6,10,14,18,22])
-ax.set_xticklabels(['SBS96A', 'SBS96B', 'SBS96C', 'SBS96D', 'SBS96E', 'SBS96F'], ha='right', rotation=30)
+ax.set_xticks([4*x+2 for x in np.arange(sbs.shape[1])])
+ax.set_xticklabels(sbs.columns, ha='right', rotation=30)
 ax.set_ylabel('Signature Exposure Levels')
 ax.set_title('SBS Mutational Signature Exposure by ER status')
 ax.legend(['Controls', 'ER Positive', 'ER Negative'], loc='upper right')
@@ -102,33 +90,34 @@ plt.close()
 
 ## DBS
 
-dbs = data['De Novo']['DBS78']
+dbs = data[sig_type]['DBS78']
 
 fig, ax = plt.subplots(figsize=(3,4))
 
-pos = [1,2,3,5,6,7]
-violins = ax.violinplot(
-    [dbs.iloc[labels_all['ER'].values=='Control',:]['DBS78A'].values,
-    dbs.iloc[labels_all['ER'].values=='0% Negative',:]['DBS78A'].values,
-    dbs.iloc[labels_all['ER'].values=='10% Positive',:]['DBS78A'].values,
+pos = np.arange(dbs.shape[1] * 4)
+pos = pos[pos % 4 != 0]  # define positions to plot (this will look like [1,2,3,5,6,7,...])
 
-    dbs.iloc[labels_all['ER'].values=='Control',:]['DBS78B'].values,
-    dbs.iloc[labels_all['ER'].values=='0% Negative',:]['DBS78B'].values,
-    dbs.iloc[labels_all['ER'].values=='10% Positive',:]['DBS78B'].values],
-    pos, widths=0.7, showmedians=True, showextrema=True
+plot_data = []
+
+for signame in dbs.columns:
+    for bio_group in ['Control', '0% Negative', '10% Positive']:
+        plot_data.append(dbs.iloc[labels_all['ER'].values==bio_group,:][signame].values)
+
+violins = ax.violinplot(
+    plot_data, pos, widths=0.7, showmedians=True, showextrema=True
 )
 
-for i in [0,3]:
+for i in [x for x in range(len(pos)) if x % 3 == 0]:
     violins['bodies'][i].set_facecolor('blue')
 
-for i in [1,4]:
+for i in [x for x in range(len(pos)) if x % 3 == 1]:
     violins['bodies'][i].set_facecolor('green')
 
-for i in [2,5]:
+for i in [x for x in range(len(pos)) if x % 3 == 2]:
     violins['bodies'][i].set_facecolor('orange')
 
-ax.set_xticks([2,6])
-ax.set_xticklabels(['DBS78A', 'DBS78B'], ha='right', rotation=30)
+ax.set_xticks([4*x+2 for x in np.arange(dbs.shape[1])])
+ax.set_xticklabels(dbs.columns, ha='right', rotation=30)
 ax.set_ylabel('Signature Exposure Levels')
 ax.set_title('DBS Exposure')
 #ax.legend(['Controls', 'ER Positive', 'ER Negative'], loc='upper right')
@@ -139,45 +128,34 @@ plt.close()
 
 ## ID83
 
-indel = data['De Novo']['ID83']
+indel = data[sig_type]['ID83']
 
 fig, ax = plt.subplots(figsize=(10,4))
 
-pos = [1,2,3, 5,6,7, 9,10,11, 13,14,15, 17,18,19]
+pos = np.arange(indel.shape[1] * 4)
+pos = pos[pos % 4 != 0]  # define positions to plot (this will look like [1,2,3,5,6,7,...])
+
+plot_data = []
+
+for signame in indel.columns:
+    for bio_group in ['Control', '0% Negative', '10% Positive']:
+        plot_data.append(indel.iloc[labels_all['ER'].values==bio_group,:][signame].values)
+
 violins = ax.violinplot(
-    [indel.iloc[labels_all['ER'].values=='Control',:]['ID83A'].values,
-    indel.iloc[labels_all['ER'].values=='0% Negative',:]['ID83A'].values,
-    indel.iloc[labels_all['ER'].values=='10% Positive',:]['ID83A'].values,
-
-    indel.iloc[labels_all['ER'].values=='Control',:]['ID83B'].values,
-    indel.iloc[labels_all['ER'].values=='0% Negative',:]['ID83B'].values,
-    indel.iloc[labels_all['ER'].values=='10% Positive',:]['ID83B'].values,
-
-    indel.iloc[labels_all['ER'].values=='Control',:]['ID83C'].values,
-    indel.iloc[labels_all['ER'].values=='0% Negative',:]['ID83C'].values,
-    indel.iloc[labels_all['ER'].values=='10% Positive',:]['ID83C'].values,
-
-    indel.iloc[labels_all['ER'].values=='Control',:]['ID83D'].values,
-    indel.iloc[labels_all['ER'].values=='0% Negative',:]['ID83D'].values,
-    indel.iloc[labels_all['ER'].values=='10% Positive',:]['ID83D'].values,
-
-    indel.iloc[labels_all['ER'].values=='Control',:]['ID83E'].values,
-    indel.iloc[labels_all['ER'].values=='0% Negative',:]['ID83E'].values,
-    indel.iloc[labels_all['ER'].values=='10% Positive',:]['ID83E'].values],
-    pos, widths=0.7, showmedians=True, showextrema=True
+    plot_data, pos, widths=0.7, showmedians=True, showextrema=True
 )
 
-for i in [0,3,6,9,12]:
+for i in [x for x in range(len(pos)) if x % 3 == 0]:
     violins['bodies'][i].set_facecolor('blue')
 
-for i in [1,4,7,10,13]:
+for i in [x for x in range(len(pos)) if x % 3 == 1]:
     violins['bodies'][i].set_facecolor('green')
 
-for i in [2,5,8,11,14]:
+for i in [x for x in range(len(pos)) if x % 3 == 2]:
     violins['bodies'][i].set_facecolor('orange')
 
-ax.set_xticks([2,6,10,14,18])
-ax.set_xticklabels(['ID83A', 'ID83B', 'ID83C', 'ID83D', 'ID83E'], ha='right', rotation=30)
+ax.set_xticks([4*x+2 for x in np.arange(indel.shape[1])])
+ax.set_xticklabels(indel.columns, ha='right', rotation=30)
 ax.set_ylabel('Signature Exposure Levels')
 ax.set_title('INDEL Mutational Signature Exposure by ER status')
 ax.legend(['Controls', 'ER Positive', 'ER Negative'], loc='upper right')
