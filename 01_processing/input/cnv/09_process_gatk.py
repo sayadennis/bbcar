001e75e6252d4dacb6487c49587d7303c771e7df
sayadennis <sayarenedennis@gmail.com>
2024-03-14 18:18:39 -0500

    add gistic2 to cnv pipeline

---
+++ 01_processing/input/cnv/09_process_gatk.py
@@ -0,0 +1,41 @@
+# pylint: disable=missing-module-docstring
+
+import glob
+import itertools
+import sys
+
+import pandas as pd
+
+din = sys.argv[
+    1
+]  # "/projects/b1131/saya/bbcar/data/02b_cnv/07_called_cn_segs/tissue_only"
+
+allmx = pd.DataFrame(
+    None,
+    columns=[
+        "name",
+        "CONTIG",
+        "START",
+        "END",
+        "NUM_POINTS_COPY_RATIO",
+        "MEAN_LOG2_COPY_RATIO",
+    ],
+)
+
+for fn in glob.glob(din + "/*.called.seg"):
+    sample_id = fn.split("/")[-1].split(".")[0]  # e.g. '1004'
+    # Count number of rows to skip (those starting with "@")
+    with open(fn, "r", encoding="utf-8") as f:
+        num_skip = sum(line.startswith("@") for line in f.readlines())
+    # Read segment file
+    data = pd.read_csv(fn, sep="\t", skiprows=num_skip)
+    # Select necessary columns, add sample name column and concatenate
+    data = data.iloc[:, [colname in allmx.columns for colname in data.columns]]
+    data["name"] = list(itertools.repeat(sample_id, len(data)))
+    allmx = pd.concat((allmx, data))
+
+# GISTIC and GATK uses different log2 standards - sub 1
+allmx["MEAN_LOG2_COPY_RATIO"] = allmx["MEAN_LOG2_COPY_RATIO"] - 1
+
+# Save the combined matrix
+allmx.to_csv(f"{din}/gistic_input_all.tsv", sep="\t", header=False, index=False)
---
+++ 01_processing/input/cnv/09_run_gistic2.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#SBATCH -A b1042
+#SBATCH -p genomics
+#SBATCH -t 2:00:00
+#SBATCH --mail-user=sayarenedennis@northwestern.edu
+#SBATCH --mail-type=END,FAIL
+#SBATCH --job-name="gistic"
+#SBATCH --output=/projects/b1131/saya/bbcar/out/03_run_gistic2_conf90.out
+
+module purge all
+module load python-miniconda3/4.12.0
+source activate bbcarenv
+
+###############################
+#### Process GATK Segments ####
+###############################
+
+python 09_process_gatk.py "/projects/b1131/saya/bbcar/data/02b_cnv/07_called_cn_segs/tissue_only"
+
+#####################
+#### Run GISTIC2 ####
+#####################
+
+din="/projects/b1131/saya/bbcar/data/02b_cnv/07_called_cn_segs/tissue_only"
+dout="/projects/b1131/saya/bbcar/data/02b_cnv/09_gistic2_out_conf90"
+
+cd /projects/p30791/GISTIC2/
+
+refgenefile="./refgenefiles/hg38.UCSC.add_miR.160920.refgene.mat"
+
+./gistic2 \
+    -b $dout \
+    -seg $din/gistic_input_all.tsv \
+    -refgene $refgenefile \
+    -genegistic 0 \
+    -smallmem 0 \
+    -brlen 0.98 \
+    -conf 0.90 \
+    -armpeel 0 \
+    -savegene 1 \
+    -gcm extreme
+
+###############################
+#### Process GATK Segments ####
+###############################
+
+# python 09_gistic_to_model_features.py
+
+
