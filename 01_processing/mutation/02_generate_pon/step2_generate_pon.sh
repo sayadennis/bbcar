cbbd2d3395f42293e4a2276d28c208596e0ab650
sayadennis <sayarenedennis@gmail.com>
2024-04-23 13:38:38 -0500

    mutation PON with containerized GATK; rm old scripts

--- 01_processing/mutation/02_generate_pon/step2_generate_pon.sh
+++ 01_processing/mutation/02_generate_pon/step2_generate_pon.sh
@@ -7,41 +7,48 @@
 #SBATCH --mail-user=sayarenedennis@northwestern.edu
 #SBATCH --mail-type=END,FAIL
 #SBATCH --job-name="pon"
-#SBATCH --output=/projects/b1131/saya/bbcar/out/generate_pon_step2.out
+#SBATCH --output=/projects/b1131/saya/new_bbcar/out/generate_pon_step2.out
 
-cd /projects/b1131/saya/bbcar/
+cd /projects/b1042/lyglab/saya/
 
-din=/projects/b1131/saya/bbcar/data/01_alignment/germline/aligned
-dout=/projects/b1131/saya/bbcar/data/02a_mutation/02_variant_calls/germline_only
+din='/projects/b1131/saya/new_bbcar/data/01_alignment/germline/aligned'
+dout='/projects/b1131/saya/new_bbcar/data/02a_mutation/02_variant_calls/germline_only'
 
 interval='/projects/b1122/gannon/bbcar/RAW_data/int_lst/SureSelect_v6/hg38.preprocessed.interval_list'
 ref='/projects/p30791/hg38_ref/hg38.fa'
 
 module purge all
-export PATH="/projects/b1131/saya/bbcar/tools/gatk-4.2.5.0:$PATH"
-# module load gatk/4.1.0
+module load singularity
+
+gatk() {
+    singularity exec -B /projects:/projects /projects/p30791/gatk_4.5.0.sif gatk "$@"
+}
 
 # Instructions taken from https://gatk.broadinstitute.org/hc/en-us/articles/360035531132
 
-# # # Step 1: Run Mutect2 in tumor-only mode for each normal sample:
-# for sampleid in $(cat /projects/b1131/saya/bbcar/sample_names_germline.txt); do 
-#     gatk Mutect2 -R $ref -I $din/${sampleid}_bqsr.bam --max-mnp-distance 0 -O $dout/${sampleid}_bqsr.vcf.gz;
-# done
-# #
-
 # Step 2: Create a GenomicsDB from the normal Mutect2 calls:
-rm -r /projects/b1042/lyglab/saya/bbcar/pon_tmp/*
-rm -rf /projects/b1042/lyglab/saya/bbcar/pon_db
-gatk GenomicsDBImport -R $ref -L $interval --tmp-dir /projects/b1042/lyglab/saya/bbcar/pon_tmp/ --genomicsdb-workspace-path /projects/b1042/lyglab/saya/bbcar/pon_db $(for x in $(ls -1 $dout/*_bqsr.vcf.gz); do echo -n "-V $x "; done)
-#
+PON_TMP='/projects/b1042/lyglab/saya/new_bbcar/pon_tmp'
+PON_DB='/projects/b1042/lyglab/saya/new_bbcar/pon_db'
+
+mkdir -p ${PON_TMP}
+mkdir -p ${PON_DB}
+
+rm -r /projects/b1042/lyglab/saya/new_bbcar/pon_tmp/*
+rm -rf /projects/b1042/lyglab/saya/new_bbcar/pon_db
+
+gatk GenomicsDBImport \
+    -R $ref \
+    -L $interval \
+    --tmp-dir ${PON_TMP}/ \
+    --genomicsdb-workspace-path ${PON_DB} \
+    $(for x in $(ls -1 $dout/*_bqsr.vcf.gz); do echo -n "-V $x "; done)
 
 # Step 3: Combine the normal calls using CreateSomaticPanelOfNormals:
-cd /projects/b1042/lyglab/saya/bbcar/
+cd /projects/b1042/lyglab/saya/new_bbcar/
 gatk CreateSomaticPanelOfNormals \
     -V gendb://pon_db \
     -R $ref \
     --output $dout/bbcar_pon.vcf.gz
-#
 
 rm -rf /projects/b1042/lyglab/saya/bbcar/pon_db
 
