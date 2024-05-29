7050b8e28232b3ce84c6ba8c7eb6515914b7d378
sayadennis <sayarenedennis@gmail.com>
2024-05-29 16:04:51 -0500

    find case- or control-unique regions and overlapping genes

--- 02_data_summary/cnv/compare_case_control_gistic.py
+++ 02_data_summary/cnv/compare_case_control_gistic.py
@@ -1,6 +1,12 @@
+import itertools
+
 import pandas as pd
 
 din = "/projects/b1131/saya/new_bbcar/data/02b_cnv"
+
+######################################
+#### Load regions and their genes ####
+######################################
 
 regions = {
     group: pd.read_csv(
@@ -11,6 +17,20 @@ regions = {
 
 for group, df in regions.items():
     regions[group] = df.iloc[df["q values"].values < 0.01, :]
+    regions[group] = df.iloc[
+        [x.endswith(" - CN values") for x in df["Unique Name"].values], :
+    ]
+    regions[group]["chrom"] = [
+        x.split(":")[0] for x in regions[group]["Wide Peak Limits"].values
+    ]
+    regions[group]["start"] = [
+        int(x.split(":")[1].split("-")[0])
+        for x in regions[group]["Wide Peak Limits"].values
+    ]
+    regions[group]["end"] = [
+        int(x.split(":")[1].split("-")[1].split("(")[0])
+        for x in regions[group]["Wide Peak Limits"].values
+    ]
 
 region_genes = {
     group: {
@@ -22,15 +42,67 @@ region_genes = {
     for group in ["case", "control"]
 }
 
-group = "case"
-trend = "del"
-cytobands = ["9q13", "10q11.23", "10q26.13", "15q13.3"]
-for cytoband in cytobands:
-    df = region_genes[group][trend]
-    aber_genes = (
-        df.iloc[:, [x.startswith(cytoband) for x in df.columns]]
-        .dropna()
-        .iloc[4:]
-        .values.ravel()
-    )
-    print(cytoband, aber_genes)
+################################################################################
+#### For visually identified case- or control-unique cytobands, print genes ####
+################################################################################
+
+for group, trend, cytobands in zip(
+    ["case", "case", "control"],
+    ["del", "amp", "amp"],
+    [
+        ["9q13", "10q11.23", "10q26.13", "15q13.3"],
+        ["4p16.3", "2q11.2", "2p11.2", "6q14.1", "7p22.1", "14q11.2"],
+        ["11p15.5"],
+    ],
+):
+    print(f"\n\n{group} - {trend}")
+    for cytoband in cytobands:
+        df = region_genes[group][trend]
+        aber_genes = (
+            df.iloc[:, [x.startswith(cytoband) for x in df.columns]]
+            .dropna()
+            .iloc[4:]
+            .values.ravel()
+        )
+        print(cytoband, aber_genes)
+
+################################################################
+#### Find case- or control-unique cytobands, identify genes ####
+################################################################
+
+genes = {
+    "case": {"amp": [], "del": []},
+    "control": {"amp": [], "del": []},
+}
+
+for trend in ["amp", "del"]:
+    case_cytos = set(x.strip() for x in regions["case"]["Descriptor"])
+    control_cytos = set(x.strip() for x in regions["control"]["Descriptor"])
+    case_unique = case_cytos - control_cytos
+    control_unique = control_cytos - case_cytos
+    # Handle cases
+    df = region_genes["case"][trend]
+    aber_genes = [
+        (
+            df.iloc[:, [x.startswith(cytoband) for x in df.columns]]
+            .dropna()
+            .iloc[4:]
+            .values.ravel()
+        )
+        for cytoband in case_unique
+    ]
+    aber_genes = list(itertools.chain.from_iterable(aber_genes))
+    genes["case"][trend] = aber_genes
+    # Handle controls
+    df = region_genes["control"][trend]
+    aber_genes = [
+        (
+            df.iloc[:, [x.startswith(cytoband) for x in df.columns]]
+            .dropna()
+            .iloc[4:]
+            .values.ravel()
+        )
+        for cytoband in control_unique
+    ]
+    aber_genes = list(itertools.chain.from_iterable(aber_genes))
+    genes["control"][trend] = aber_genes
