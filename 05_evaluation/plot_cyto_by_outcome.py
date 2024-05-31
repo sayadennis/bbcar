5dc7680d716487540c6970db56a16f55f8f12cb8
sayadennis <sayarenedennis@gmail.com>
2024-05-31 15:54:30 -0500

    plot shap values; plot cytoband-level aberration by outcome for slides

---
+++ 05_evaluation/plot_cyto_by_outcome.py
@@ -0,0 +1,75 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+dout = "/projects/b1131/saya/new_bbcar/plots/cnv"
+
+cn_data = {
+    "amp": pd.read_csv(
+        "/projects/b1131/saya/new_bbcar/data/02b_cnv/10_cleaned_cnv/cyto_thres_amp.csv",
+        index_col=0,
+    ),
+    "del": pd.read_csv(
+        "/projects/b1131/saya/new_bbcar/data/02b_cnv/10_cleaned_cnv/cyto_thres_del.csv",
+        index_col=0,
+    ),
+}
+
+for key, df in cn_data.items():
+    cn_data[key] = (df != 0).astype(float)  # change to binary
+
+labels = pd.read_csv("/projects/b1131/saya/new_bbcar/label_all.csv", index_col=0)
+labels = labels.loc[cn_data["amp"].index, :]
+
+amp_colors = [matplotlib.colormaps["Reds"](index) for index in np.linspace(0, 1, 3)]
+del_colors = [matplotlib.colormaps["Blues"](index) for index in np.linspace(0, 1, 3)]
+
+for cyto in ["1q21.1", "16p12.3", "6q14.1", "10q11.22"]:
+    plot_data = {}
+
+    for aber_type in ["amp", "del"]:
+        data = pd.DataFrame(columns=["case", "control"], index=[0, 1])
+        for intlabel, label in enumerate(["control", "case"]):
+            subdata = cn_data[aber_type][cyto].loc[
+                labels.iloc[labels.values == intlabel, :].index
+            ]
+            for aber in [0, 1]:
+                data.loc[aber, label] = (
+                    abs(subdata.values) == aber
+                ).sum() / subdata.shape[0]
+        plot_data[aber_type] = data
+
+    fig, ax = plt.subplots(figsize=(2, 2.5))
+
+    ax.bar(
+        [1, 2],
+        [
+            plot_data["amp"]["case"].loc[1],
+            plot_data["amp"]["control"].loc[1],
+        ],
+        edgecolor="black",
+        color=amp_colors[1],
+        label="Amp",
+    )
+    ax.bar(
+        [4, 5],
+        [plot_data["del"]["case"].loc[1], plot_data["del"]["control"].loc[1]],
+        edgecolor="black",
+        color=del_colors[1],
+        label="Del",
+    )
+
+    ax.set_xticks([1, 2, 4, 5])
+    ax.set_xticklabels(["Case", "Control", "Case", "Control"], rotation=45, ha="right")
+    ax.set_ylabel("Proportion")
+    ax.set_ylim(0, 0.75)
+    ax.spines["right"].set_visible(False)
+    ax.spines["top"].set_visible(False)
+    if cyto != "1q21.1":
+        ax.legend()
+    ax.set_title(cyto)
+
+    plt.tight_layout()
+    fig.savefig(f"{dout}/amp_del_proportions_{cyto}.png")
+    plt.close()
--- 05_evaluation/plot_feature_importance.py
+++ 05_evaluation/plot_feature_importance.py
@@ -1,10 +1,19 @@
+import pickle
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import shap
 
 din = "/projects/b1131/saya/new_bbcar/model_interpretations/breast_cancer_prediction"
 dout = "/projects/b1131/saya/new_bbcar/plots"
+
+############################################
+#### Plot XGB feature importance scores ####
+############################################
+
+top_n = 5
 
 f_imp = {
     "Mutation": pd.read_csv(
@@ -23,22 +32,22 @@ colors = {
 }
 
 for feature in ["Mutation", "CNV"]:
-    top10_features = f_imp[feature].columns[
-        (np.argsort(-1 * f_imp[feature].mean(axis=0)))[:10]
+    top_features = f_imp[feature].columns[
+        (np.argsort(-1 * f_imp[feature].mean(axis=0)))[:top_n]
     ]
     plot_data = pd.melt(
-        f_imp[feature][top10_features], var_name="Feature", value_name="Importance"
+        f_imp[feature][top_features], var_name="Feature", value_name="Importance"
     )
     # Plot importance scores
-    fig, ax = plt.subplots(figsize=(8, 4))
-    sns.swarmplot(
+    fig, ax = plt.subplots(figsize=(4.0, 3.5))
+    sns.stripplot(
         data=plot_data,
         x="Feature",
         y="Importance",
         color=colors[feature],
         ax=ax,
     )
-    ax.set_title(f"Feature Importance Scores: Top 10 of {feature}", fontsize=14)
+    # ax.set_title(f"Feature Importance Scores: Top {top_n} of {feature}", fontsize=14)
     ax.set_ylim(-0.005, None)
     ax.set_xlabel("")
     ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha="right")
@@ -46,4 +55,26 @@ for feature in ["Mutation", "CNV"]:
     ax.spines["top"].set_visible(False)
     plt.tight_layout()
     fig.savefig(f"{dout}/top_feature_importance_{feature.lower()}.png")
+    plt.close()
+
+####################
+#### SHAP plots ####
+####################
+
+for category, filename, top_features in zip(
+    ["cnv", "mutation"],
+    ["compare_gistic/results_cyto_thres_aber_shap.p", "no_fusion_mutation_orig_shap.p"],
+    [
+        ["1q21.1", "16p12.3", "15q11.2", "1p36.13", "17p12", "6q14.1", "10q11.22"],
+        ["T[T>C]C", "A[T>G]T", "A[T>C]C", "A[C>G]T", "G[C>G]G", "T[C>A]T", "T[C>A]C"],
+    ],
+):
+    with open(f"{din}/{filename}", "rb") as f:
+        shap_values = pickle.load(f)
+    # Filter shap values by top importance features
+    filtered_shap_values = shap_values[:, top_features]
+    # plot
+    shap.plots.beeswarm(filtered_shap_values)
+    plt.tight_layout()
+    plt.savefig(f"{dout}/shap_{category}_xgb.png")
     plt.close()
