54edd5a2abba50125a787c38242e3326eb0068f0
sayadennis <sayarenedennis@gmail.com>
2024-09-25 13:06:27 -0500

    plot PCA colored by difficult-to-predict samples

--- 02_data_summary/mutation/clustermap_mutsig_features.py
+++ 02_data_summary/mutation/clustermap_mutsig_features.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 sns.set_theme(color_codes=True)
-from sklearn.preprocessing import StandardScaler
 
 proj_dir = "/projects/b1131/saya/new_bbcar"
 sig_results_dir = f"{proj_dir}/data/02a_mutation/08_feature_matrix"
@@ -21,12 +23,15 @@ meta = pd.read_csv("/projects/b1131/saya/new_bbcar/meta.csv")
 meta = meta.iloc[(meta.tissue.values == 1) & (meta.seq_ctrl.values == 0), :]
 meta.set_index("patient_id", inplace=True)
 
+impossibles = [551, 1091, 1615, 1624, 694, 1220, 710, 714, 719, 763, 1324, 1382, 1402]
+meta["impossible"] = np.array([x in impossibles for x in meta.index]).astype(float)
+
 ## Plot heatmap
 
 palette = sns.color_palette("colorblind")
 
 for valtype, mx in data.items():
-    for color_label in ["batch", "label"]:
+    for color_label in ["batch", "label", "impossible"]:
         scaler = StandardScaler()
         mx = pd.DataFrame(scaler.fit_transform(mx), index=mx.index, columns=mx.columns)
         # colors
@@ -43,3 +48,29 @@ for valtype, mx in data.items():
         )
         plt.savefig(f"{dout}/cluster_heatmap_{valtype}_color{color_label}.png")
         plt.close()
+
+##################
+#### Plot PCA ####
+##################
+
+pca = PCA(n_components=2)
+data_tf = pd.DataFrame(
+    pca.fit_transform(data["SBS96"]), index=data["SBS96"].index, columns=np.arange(2)
+)
+
+fig, ax = plt.subplots()
+
+ax.scatter(
+    data_tf.iloc[[x not in impossibles for x in data_tf.index], 0],
+    data_tf.iloc[[x not in impossibles for x in data_tf.index], 1],
+    color="c",
+)
+ax.scatter(
+    data_tf.iloc[[x in impossibles for x in data_tf.index], 0],
+    data_tf.iloc[[x in impossibles for x in data_tf.index], 1],
+    color="m",
+)
+
+plt.tight_layout()
+fig.savefig("/home/srd6051/test_pca.png")
+plt.close()
