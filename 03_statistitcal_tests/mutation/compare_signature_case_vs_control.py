5a68ba6d9c1957b5d18ffd709a8ef374a6343678
sayadennis <sayarenedennis@gmail.com>
2024-09-25 13:09:42 -0500

    change to rank sum test; update project directory

--- 03_statistitcal_tests/mutation/compare_signature_case_vs_control.py
+++ 03_statistitcal_tests/mutation/compare_signature_case_vs_control.py
@@ -1,88 +1,115 @@
-import glob
-import numpy as np
 import pandas as pd
-from scipy.stats import ttest_ind
-import seaborn as sns
-import matplotlib.pyplot as plt
+from scipy.stats import ranksums
 
-datadir = '/projects/b1131/saya/bbcar/data'
-mutdir = f'{datadir}/02a_mutation/08_feature_matrix'
-vardir = f'{datadir}/02a_mutation/02_variant_calls'
-dout_stat = '/projects/b1131/saya/bbcar/out'
-dout_plot = '/projects/b1131/saya/bbcar/plots'
+projdir = "/projects/b1131/saya/new_bbcar"
+mutdir = f"{projdir}/data/02a_mutation/08_feature_matrix"
+vardir = f"{projdir}/data/02a_mutation/02_variant_calls"
+dout_stat = f"{projdir}/out"
+dout_plot = f"{projdir}/plots"
 
 ######################################
 #### Get case/control assignments ####
 ######################################
 
-label = pd.read_csv(f'{datadir}/clinical/bbcar_label_studyid_from_gatk_filenames.csv', index_col=0)
-redcap_label = pd.read_csv(f'{datadir}/clinical/bbcar_redcap_label_studyid.csv', index_col=0)
-sample_ids = pd.read_csv(f'{mutdir}/denovo_signature_per_sample_SBS96.csv', index_col=0).index
-for sample_id in sample_ids:
-    if sample_id not in label.index:
-        label = pd.concat((
-            label, 
-            pd.DataFrame([redcap_label.loc[sample_id,'cc_id']], index=[sample_id], columns=['label'])
-        ))
+label = pd.read_csv(f"{projdir}/label_all.csv", index_col=0)
+sample_ids = pd.read_csv(
+    f"{mutdir}/denovo_signature_per_sample_SBS96.csv", index_col=0
+).index
 
 ###############################################
 #### Loop through sigs and perform t-tests ####
 ###############################################
 
-stat_results = pd.DataFrame(columns=['Signature Type', 'Signature name', 't statistic', 'p-value'])
+stat_results = pd.DataFrame(
+    columns=["Signature Type", "Signature name", "statistic", "p-value"]
+)
 
-for sigtype in ['SBS96', 'DBS78', 'ID83']:
+for sigtype in ["SBS96", "DBS78", "ID83"]:
     #### De Novo Signatures ####
-    signature = pd.read_csv(f'{mutdir}/denovo_signature_per_sample_{sigtype}.csv', index_col=0)
-    signature_cases = signature.loc[label.iloc[label.values==1,:].index,:]
-    signature_controls = signature.loc[label.iloc[label.values==0,:].index,:]
+    signature = pd.read_csv(
+        f"{mutdir}/denovo_signature_per_sample_{sigtype}.csv", index_col=0
+    )
+    signature_cases = signature.iloc[
+        [x in label.iloc[label.values == 1, :].index for x in signature.index], :
+    ]
+    signature_controls = signature.iloc[
+        [x in label.iloc[label.values == 0, :].index for x in signature.index], :
+    ]
     #
-    signature_cases_ratio = signature_cases.divide(signature_cases.sum(axis=1), axis='rows')
-    signature_controls_ratio = signature_controls.divide(signature_controls.sum(axis=1), axis='rows')
-    # 
+    signature_cases_ratio = signature_cases.divide(
+        signature_cases.sum(axis=1), axis="rows"
+    )
+    signature_controls_ratio = signature_controls.divide(
+        signature_controls.sum(axis=1), axis="rows"
+    )
+    #
     # print('\n#### Results for raw values ####')
-    results = ttest_ind(signature_cases, signature_controls, axis=0)
+    results = ranksums(signature_cases, signature_controls, axis=0)
     for signame, t, p in zip(signature.columns, results.statistic, results.pvalue):
-        stat_results = pd.concat((
-            stat_results, 
-            pd.DataFrame({
-                'Signature Type' : f'{sigtype} De Novo',
-                'Signature name' : signame,
-                't statistic' : t,
-                'p-value' : p,
-            }, index=[0])
-        ))
-    # 
+        stat_results = pd.concat(
+            (
+                stat_results,
+                pd.DataFrame(
+                    {
+                        "Signature Type": f"{sigtype} De Novo",
+                        "Signature name": signame,
+                        "statistic": t,
+                        "p-value": p,
+                    },
+                    index=[0],
+                ),
+            )
+        )
+    #
     # print('\n#### Results for ratios ####')
-    # results = ttest_ind(signature_cases_ratio, signature_controls_ratio, axis=0)
+    # results = ranksums(signature_cases_ratio, signature_controls_ratio, axis=0)
     # for signame, t, p in zip(signature.columns, results.statistic, results.pvalue):
     #     print(f'{signame}: {t:.2f} (p={p:.4f})')
     # print('\n')
     #### Decomposed to COSMIC Signatures ####
-    signature = pd.read_csv(f'{mutdir}/cosmic_signature_per_sample_{sigtype}.csv', index_col=0)
-    signature_cases = signature.loc[label.iloc[label.values==1,:].index,:]
-    signature_controls = signature.loc[label.iloc[label.values==0,:].index,:]
+    signature = pd.read_csv(
+        f"{mutdir}/cosmic_signature_per_sample_{sigtype}.csv", index_col=0
+    )
+    signature_cases = signature.iloc[
+        [x in label.iloc[label.values == 1, :].index for x in signature.index], :
+    ]
+    signature_controls = signature.iloc[
+        [x in label.iloc[label.values == 0, :].index for x in signature.index], :
+    ]
     #
-    signature_cases_ratio = signature_cases.divide(signature_cases.sum(axis=1), axis='rows')
-    signature_controls_ratio = signature_controls.divide(signature_controls.sum(axis=1), axis='rows')
-    # 
+    signature_cases_ratio = signature_cases.divide(
+        signature_cases.sum(axis=1), axis="rows"
+    )
+    signature_controls_ratio = signature_controls.divide(
+        signature_controls.sum(axis=1), axis="rows"
+    )
+    #
     # print('\n#### Results for raw values ####')
-    results = ttest_ind(signature_cases, signature_controls, axis=0)
+    results = ranksums(signature_cases, signature_controls, axis=0)
     for signame, t, p in zip(signature.columns, results.statistic, results.pvalue):
-        stat_results = pd.concat((
-            stat_results, 
-            pd.DataFrame({
-                'Signature Type' : f'{sigtype} COSMIC',
-                'Signature name' : signame,
-                't statistic' : t,
-                'p-value' : p,
-            }, index=[0])
-        ))
-    # 
+        stat_results = pd.concat(
+            (
+                stat_results,
+                pd.DataFrame(
+                    {
+                        "Signature Type": f"{sigtype} COSMIC",
+                        "Signature name": signame,
+                        "statistic": t,
+                        "p-value": p,
+                    },
+                    index=[0],
+                ),
+            )
+        )
+    #
     # print('\n#### Results for ratios ####')
-    # results = ttest_ind(signature_cases_ratio, signature_controls_ratio, axis=0)
+    # results = ranksums(signature_cases_ratio, signature_controls_ratio, axis=0)
     # for signame, t, p in zip(signature.columns, results.statistic, results.pvalue):
     #     print(f'{signame}: {t:.2f} (p={p:.4f})')
     # print('\n')
 
-stat_results.to_csv(f'{dout_stat}/mutational_signature_exposure_ttests.csv', header=True, index=False)
+stat_results.iloc[stat_results["p-value"].values < 0.05, :].to_csv(
+    f"{dout_stat}/mutational_signature_exposure_ranksum_significant.csv",
+    header=True,
+    index=False,
+)
